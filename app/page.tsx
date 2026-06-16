3fd17368ec38bcf85da68f0024169240c233e118
Catalina <catalinamcquade@gmail.com>
2026-06-16 01:09:44 -0400

    added sass to project

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
 import ClippyWrapper from './components/clippy-wrapper'
 
+import './globals.scss'
+
 export default function Home() {
   return (
-    <div>
-      <ClippyWrapper />
+    <div className="home">
+      {/* TODO: Remove Clippy package and component entirely eventually */}
+      {/* <ClippyWrapper /> */}
       Hello!
     </div>
   )
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+  devIndicators: false,
+  sassOptions: {
+    implementation: 'sass-embedded',
+  },
+}
 
-export default nextConfig;
+export default nextConfig
